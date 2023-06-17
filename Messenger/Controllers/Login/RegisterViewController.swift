535803e21775e62ae0d7dd126acf5da8670b66e4
Gabriel Castro <gabrieldcc@gmail.com>
2023-06-17 16:13:36 -0300

    picker photo adjusted

--- Messenger/Controllers/Login/RegisterViewController.swift
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -23,8 +23,8 @@ class RegisterViewController: UIViewController {
         )
         setupSubView()
         setupActionButtons()
-        //        emailField.delegate = self
-        //        passwordField.delegate = self
+        emailField.delegate = self
+        passwordField.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -35,6 +35,7 @@ class RegisterViewController: UIViewController {
                                  y: 20,
                                  width: size,
                                  height: size)
+        imageView.layer.cornerRadius = imageView.width/2.0
         firstNameField.frame = CGRect(x: 30,
                                   y: imageView.bottom + 10,
                                   width: scrollView.width-60,
@@ -70,6 +71,9 @@ class RegisterViewController: UIViewController {
         component.image = UIImage(systemName: "person")
         component.tintColor = .gray
         component.contentMode = .scaleAspectFit
+        component.layer.masksToBounds = true
+        component.layer.borderColor = UIColor.lightGray.cgColor
+        component.layer.borderWidth = 1
         return component
     }()
     
@@ -258,9 +262,11 @@ extension RegisterViewController: UIImagePickerControllerDelegate, UINavigationC
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        print(info)
-//        let selectedImage = 
-//        self.imageView.image = selectedImage
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage]  as? UIImage else {
+            return
+        }
+        self.imageView.image = selectedImage
+        picker.dismiss(animated: true)
     }
 
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
